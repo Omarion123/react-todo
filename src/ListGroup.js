4763e7c0a37a928d95328a8cbe48156ce297282c
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 <anzymen@gmail.com>
2023-10-08 16:22:47 +0200

    new todo list

--- src/ListGroup.js
+++ src/ListGroup.js
@@ -1,14 +1,23 @@
-const ListGroup = () => {
-  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-  items = [];
+import { useState } from "react";
 
+const ListGroup = () => {
+  const items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>List Group</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={selectedIndex === index ? "active" : null}
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
